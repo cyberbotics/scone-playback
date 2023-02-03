97d7e676a6a71d0192475463ec32b6b95c8d0d60
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-02-03 08:03:49 +0100

    version and '

--- convert.py
+++ convert.py
@@ -161,13 +161,13 @@ file = minidom.parseString(content.replace('::', ''))
 
 bones = file.getElementsByTagName('Body')
 
-x3d = """<?xml version='1.0' encoding='UTF-8'?>
-<!DOCTYPE X3D PUBLIC 'ISO//Web3D//DTD X3D 3.0//EN' 'http://www.web3d.org/specifications/x3d-3.0.dtd'>
-<X3D version='3.0' profile='Immersive' xmlns:xsd='http://www.w3.org/2001/XMLSchema-instance'
- xsd:noNamespaceSchemaLocation='http://www.web3d.org/specifications/x3d-3.0.xsd'>
+x3d = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.0//EN" "http://www.web3d.org/specifications/x3d-3.0.dtd">
+<X3D version="3.0" profile="Immersive" xmlns:xsd="http://www.w3.org/2001/XMLSchema-instance"
+ xsd:noNamespaceSchemaLocation="http://www.web3d.org/specifications/x3d-3.0.xsd">
 <head>
-<meta name='generator' content='Webots' />
-<meta name='version' content='R2022b' />
+<meta name="generator" content="Webots" />
+<meta name="version" content="R2023b" />
 </head>
 <Scene>
 <WorldInfo id='n1' title='Human Skeleton' info='Imported from OpenSim' basicTimeStep='32' coordinateSystem='ENU'>
